19ddbfcbd8d309094977db5c38e7b45bde94390e
PazitoPazos <javier.pazos001@gmail.com>
2022-01-28 10:15:52 +0100

    Ejercicio 5

---
+++ ejercicio_5.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Ejercicio 5</title>
+    </head>
+    <body>
+        <h3>Ejercicio 5</h3>
+        <p>Realiza un programa que resuelva una ecuación de primer grado (del tipo ax + b = 0).</p>
+            <form action="ejercicio_5.php" method="post">
+            <p>
+                <input type="text" name="a" size="1" value="a">x + 
+                <input type="text" name="b" size="1" value="b"> = 0<br>
+            </p>
+            <p><input type="submit" value="Calcular"></p>
+            </form>
+        <?php
+            $a = $_REQUEST['a'];
+            $b = $_REQUEST['b'];
+                if ($a == "" || $b == "") {
+                    echo "<p>Inserta los valores</p>";
+                } elseif ($a == 0) {
+                    echo "Esa ecuación no tiene solución real.";
+                } else {
+                    echo "x = ", (-$b / $a);
+                }
+        ?>
+    </body>
+</html>
