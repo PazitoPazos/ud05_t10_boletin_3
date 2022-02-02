f24697bfa1fb877080bf1673d1e86a7e75547e71
PazitoPazos <javier.pazos001@gmail.com>
2022-02-02 12:30:53 +0100

    Ejercicio 5

--- ejercicio_5.php
+++ ejercicio_5.php
@@ -17,7 +17,7 @@
         <?php
             $a = $_REQUEST['a'];
             $b = $_REQUEST['b'];
-                if ($a == "" || $b == "") {
+                if (!is_numeric($a) || !is_numeric($b)) {
                     echo "<p>Inserta los valores</p>";
                 } elseif ($a == 0) {
                     echo "Esa ecuación no tiene solución real.";
