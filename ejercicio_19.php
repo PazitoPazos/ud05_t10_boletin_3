51075df18f0996295809b8e49a76bf4f8cd0c1ba
PazitoPazos <javier.pazos001@gmail.com>
2022-02-03 16:12:15 +0100

    Ejercicio 19

---
+++ ejercicio_19.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Ejercicio 19</title>
+    </head>
+    <body>
+        <h3>Ejercicio 19</h3>
+        <p>Escribe un programa que diga cuál es la primera cifra de un número entero introducido por teclado. Se permiten números de hasta 5 cifras.</p>
+            <form action="ejercicio_19.php" method="post">
+            <p>
+                <p>Número: <input type="text" name="a" size="3" value=""></p>
+            </p>
+            <p><input type="submit" value="Enviar"></p>
+            </form>
+        <?php
+            $a = $_REQUEST['a'] != "" ? intval($_REQUEST['a']) : "";
+            $splita = str_split($a);
+            $ar = implode(array_reverse($splita));
+            if (!is_int($a)) {
+                echo "<p>Introduce un valor entero</p>";
+            } elseif (strlen($a) > 5 || $a < 0) {
+                echo "<p>Sólo números enteros positivos de un máximo de 5 cifras (máx. 99999)</p>";
+            } elseif ($a == $ar) {
+                echo "<p>El número $a es capicúa</p>";
+            } else {
+                echo "<p>El número $a no es capicúa</p>";
+            }
+        ?>
+    </body>
+</html>
